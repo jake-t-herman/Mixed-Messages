4b2a1a90289b94cdc6e036c6d2dda4e37fd75306
Jake Herman <jake.t.herman@gmail.com>
2023-05-16 15:01:23 +0100

    added function to randomly select sound from array

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const Initial = [sh,ch,th,y,b,j,c,r,sm,wh,pl,m];
-const Medial = [a,e,i,o,u];
-const Final = [ch,ng,ed,n,b,x,p,nk,ff,ll,t];
+// Declare sounds in arrays
+const Initial = ['sh','sh','th','y','b','j','c','r','sm','wh','pl','m','st'];
+const Medial = ['a','e','i','o','u'];
+const Final = ['ch','ng','ed','n','b','x','p','nk','ff','ll','t'];
+
+//Function to randomly select sound from an array
+const soundSelector = batch => {
+    let sound = batch[Math.floor(Math.random()* batch.length)] ;
+    return sound;
+};
+
+
+
+console.log(soundSelector(Medial))
